167f6a5ee8e55ff49c606151ca38b337f12aad8c
ashok bharathi <bharathi17barathi@gmail.com>
2024-12-30 00:46:26 +0530

    refactor login functionality and update content display

--- js/script.js
+++ js/script.js
@@ -4,29 +4,28 @@ function login() {
     var password = document.getElementById("password").value;
 
     if (username == "Surya@vizhigal" && password == "Surya@vizhigal1234567890") {
-        alertbox.render({
-            alertIcon: 'success',
-            title: 'Thank You!',
-            message: 'You have logged in successfully.',
-            btnTitle: 'Ok',
-            border:true
-        });
-        document.getElementById("editing_post").style.display = "block";
+        document.getElementById("successfully").style.display = "block";
+        setTimeout(() => {
+            window.location.href = "content_post.html";
+        }, 3000); // 3000 milliseconds = 3 seconds
     } else {
-        alertbox.render({
-            alertIcon: 'error',
-            title: 'Thank You!',
-            message: 'Your login attempt has failed',
-            btnTitle: 'Ok',
-            border:true
-        });
+        document.getElementById("failed").style.display = "block";
     }
 }
 
 // LATEST TREND
-function test(){
-    var x = document.getElementById("testing").value;
-}
 
-var runningnews = document.getElementById("running").textContent;
-console.log(runningnews)
+
+function trent() {
+    var username = document.getElementById("userid").value;
+    var password = document.getElementById("password").value;
+    var news = document.getElementById("LATEST_TREND").value;
+    var abc = news;
+    if (username == "Surya@vizhigal" && password == "Surya@vizhigal1234567890") {
+        alert(abc);
+        document.getElementById("running").innerHTML = abc;
+    } 
+    else {
+        alert("Login failed");   
+    }
+}
